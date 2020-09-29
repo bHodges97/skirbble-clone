1308afe366b8318a4d625a2498b69dd0f532577f
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-29 21:59:47 +0100

    Canvas draws simple house on click

--- components/canvas.js
+++ components/canvas.js
@@ -3,15 +3,32 @@ import React from "react"
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
-    //this.handleClick = this.handleClick.bind(this);  }
+    this.handleClick = this.handleClick.bind(this); 
   }
 
-  handleClick() {
+  handleClick(e) {
     this.setState(state => ({}));  
+    const ctx = e.target.getContext('2d');
+    // Set line width
+    ctx.lineWidth = 10;
+
+    // Wall
+    ctx.strokeRect(75, 140, 150, 110);
+
+    // Door
+    ctx.fillRect(130, 190, 40, 60);
+
+    // Roof
+    ctx.beginPath();
+    ctx.moveTo(50, 140);
+    ctx.lineTo(150, 60);
+    ctx.lineTo(250, 140);
+    ctx.closePath();
+    ctx.stroke();
   }
   render() {
     return (
-      <canvas class="doodleArea" width="100" height="100" onClick={this.handleClick}>
+      <canvas className="doodleArea" width="800" height="600" onClick={(e)=>this.handleClick(e)}>
       </canvas>
     );
   }
--- pages/rooms/room-template.js
+++ pages/rooms/room-template.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import Canvas from '../../components/canvas'
 
 export default function Room() {
   return (
     <Layout>
       <h1>WIP</h1>
       <Link href="/"><a>Back</a></Link>
+      <Canvas/>
     </Layout>
   )
         
