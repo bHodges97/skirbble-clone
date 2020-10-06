950690110e9d6a0c67facbe4571e1ea0455cfd52
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-10-06 16:19:01 +0100

    Adding core game logic

--- server.js
+++ server.js
@@ -12,15 +12,69 @@ const fs = require('fs');
 let wordfile = fs.readFileSync('words.json');
 const wordlist = JSON.parse(wordfile).words;
 
+function random_item(items){
+	return items[Math.floor(Math.random()*items.length)];
+}
+
 class Room{
 	constructor(id){
 		this.id = id;
+		this.gameState = "lobby";
 		this.players = new Array(10);
-		this.displayWord = "";
+		this.currentPlayer = null;
 		this.word = "";
-		this.round = 1;
+		this.hiddenWord = "";
+		this.compareWord = "";
+		this.choices = ['','',''];
+		this.round = 0;
 		this.image = "";
 		this.playerCount = 0
+		this.startTime = Date.now()
+	}
+
+	updateStatus(){
+		if(this.gameState != "lobby" && this.playerCount == 1){
+			//stop game
+
+
+		}else if(this.gameState == "lobby" && this.playerCount > 1){
+			//start game
+			this.newRound();
+		}
+	}
+
+	newRound(){
+		this.round += 1;
+		for(let player of this.players){
+			player.participated = false;
+		}
+		io.to(this.id).emit("round", this.round);
+		this.selectWord();
+	}
+
+	selectWord(){
+		//select player first
+		let player = null;
+		for(let p of this.players){
+			if(p.participated){
+				player = p
+				p.participated = true;
+				break
+			}
+		}
+		if(player == null){
+			newRound();
+		}else{
+			//select three words
+			for(let i = 0; i < 3; i++){
+				this.choices[i] = random_item(wordlist);
+			}
+
+			this.currentPlayer = player;
+
+			io.to(this.id).emit('choosing', player.name);
+			io.to(player.id).emit('choice', this.choices);
+		}
 	}
 
 	addPlayer(data, id){
@@ -29,7 +83,9 @@ class Room{
 				this.players[i] = data;
 				this.players[i].score = 0;
 				this.players[i].key = id;
+				this.players[i].participated = false;
 				this.playerCount+=1
+				this.updateStatus()
 				return i
 			}
 		}
@@ -49,13 +105,19 @@ class Room{
 				let name = this.players[i].name
 				this.players[i] = undefined;
 				this.playerCount-=1
+				this.updateStatus()
 				return name;
 			}
 		}
 	}
-}
-function random_item(items){
-	return items[Math.floor(Math.random()*items.length)];
+
+	//game timer
+	start(){
+		this.startTime = Date.now()
+		io.to(this.id).emit('go', {time: this.startTime, word: this.hiddenWord});
+		setTimeout(myFunc, 1500, 'funky');
+	}
+
 }
 
 function validate(user){
@@ -81,6 +143,8 @@ io.on('connect', socket => {
 	socket.emit('now',{
 		message: 'message received',
 	})
+
+	//player connecting
 	socket.on('join', (data)=>{
 		data = validate(data)
 		if(data == false){
@@ -99,7 +163,8 @@ io.on('connect', socket => {
 		socket.to(roomcode).emit('playerjoined', room.players[id]);
 		io.to(roomcode).emit('message', {content: data.name + " joined.", color: '#56ce27'});
 	})
-
+	
+	//playing sends message in chat
 	socket.on('message', (data)=>{
 		const clientrooms = Object.keys(socket.rooms);
 		if(typeof(data) !== 'string' || data.length > 100 || clientrooms.length == 1){
@@ -124,6 +189,29 @@ io.on('connect', socket => {
 		}
 	})
 
+	//player chooses a word;
+	socket.on('choice', (data)=>{
+		if(!Number.isInteger(data) || data < 0  || data > 2 || clientrooms.length == 1){
+			socket.emit('error')
+			return
+		}
+		console.log(clientrooms);
+		let roomcode = clientrooms[1]
+		let room = rooms.get(roomcode);
+		let player = room.getPlayer(socket.id);
+		if (socket.id == room.currentPlayer){
+			room.word = room.choices[data];
+			room.hiddenWord = ''
+			for(let c of room.word){
+				room.hiddenWord += c==' '?' ':'_';
+			}
+			room.compareWord = room.displayerWord.toLowerCase();
+			room.choices = ['','',''];
+			room.start();
+		}
+	});
+	
+	//player disconnects
 	socket.on('disconnecting', () => {
 		const clientrooms = Object.keys(socket.rooms);
 		console.log("disconnecting");
