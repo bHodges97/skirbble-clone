c184ab3ec7e6656b5b47c30dc3aa85c816eea389
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-10-21 23:44:01 +0100

    gameend container added

---
+++ components/gameend.js
@@ -0,0 +1,17 @@
+
+class Gameend extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+  componentDidMount(){
+  }
+  
+
+  render() {
+    return (
+      <div className="containerGameEnd">
+      </div>
+    )
+  }
